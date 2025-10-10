17e18f79ebacfcf06e86a7e05d1fb096a9fc72f3
Ha Quanq <quangha0120010@gmail.com>
2025-10-10 10:00:09 +0700

    Add static images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+export { default as SidebarBackgroundDesktop } from "./bg-sidebar-desktop.svg";
+export { default as SidebarBackgroundMobile } from "./bg-sidebar-mobile.svg";
+export { default as AdvancedIcon } from "./icon-advanced.svg";
+export { default as CheckmarkIcon } from "./icon-checkmark.svg";
+export { default as ProIcon } from "./icon-pro.svg";
+export { default as ThanksIcon } from "./icon-thank-you.svg";
